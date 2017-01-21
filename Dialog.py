aa28dd8d7c3514216f362f3b1a2c15bde30f6302
kcaras6 <kcaras6@gatech.edu>
2017-01-21 10:05:28 -0500

    Dialog and time stamp class
    Added a few basic classed to be used to store the table data

---
+++ Dialog.py
@@ -0,0 +1,11 @@
+class Dialog:
+    def __init__(self, st, c, d, t, s, r, sa ):
+        self.speaker = s
+        self.recipient = r
+        self.stamp = st
+        self.content = c
+        self.date = d
+        self.time= t
+        self.startsAt = sa
+
+
