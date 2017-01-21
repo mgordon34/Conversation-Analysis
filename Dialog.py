405357ae79a8b6500bb37e4a8860b4d8f8ddf357
kcaras6 <caraskimberly@yahoo.com>
2017-01-21 13:20:13 -0500

    I have completed some basic positive negative sentiment analysis on the Abbott and Costello Who's on first skit. Right now all output is just printed to the command line. I used the Vader module in nltk.

--- Dialog.py
+++ Dialog.py
@@ -1,5 +1,10 @@
 import nltk.corpus
 from nltk.text import Text
+from nltk.corpus import sentiwordnet as swn
+from nltk.sentiment.util import *
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from nltk import tokenize
+
 class Dialog:
 
     def __init__(self, st, c, d, t, s, r, sa):
@@ -16,24 +21,47 @@ class Dialog:
 
 if __name__ == '__main__':
     fi = open("AbbottCostello.txt")
-    raw = fi.read()
-    tokens = nltk.word_tokenize(raw)
-    t = nltk.Text(tokens)
-    t.similar("")
-    fi.close()
-    fi = open("AbbottCostello.txt")
     cnt = 1
-    speakers = {"Abbott:": [], "Costello:": []}
+    speakers = {"Abbott": [], "Costello": []}
     for line in fi.readlines():
-        a = line.split()
+        a = line.split(":")
         di = None
         if len(a) < 1:
             continue
-        elif a[0] == "Abbott:":
-            di = Dialog(None, line[1:], None, cnt, a[0], "Costello:", None)
-        elif a[0] == "Costello:":
-            di = Dialog(None, line[1:], None, cnt, a[0], "Abbott:", None)
-        speakers[a[0]].append(di)
+        elif a[0] == "Abbott":
+            di = Dialog(None, a[1], None, cnt, a[0], "Costello:", None)
+            speakers[a[0]].append(di)
+        elif a[0] == "Costello":
+            di = Dialog(None, a[1], None, cnt, a[0], "Abbott:", None)
+            speakers[a[0]].append(di)
         cnt += 1
-    fi.close()
 
+
+
+    """
+        for k in speakers.keys():
+        print k
+        for diag in speakers[k]:
+            print diag.content
+    """
+
+    fi.close()
+    sid = SentimentIntensityAnalyzer()
+    print "++++++++++++++++++++++++++++++++++++++++++"
+    for diag in speakers["Abbott"]:
+        sentence = diag.content
+        print sentence
+        ss = sid.polarity_scores(sentence)
+        for k in sorted(ss):
+            print ('{0} : {1}, '.format(k, ss[k]))
+        print "++++++++++++++++++++++++++++++++++++++++++"
+
+    sid = SentimentIntensityAnalyzer()
+    print "*********************************************"
+    for diag in speakers["Costello"]:
+        sentence = diag.content
+        print sentence
+        ss = sid.polarity_scores(sentence)
+        for k in sorted(ss):
+            print ('{0} : {1}, '.format(k, ss[k]))
+        print "******************************************"
