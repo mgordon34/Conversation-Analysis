370fd688c69ab11a8debab5b4f44d7138379144f
kcaras6 <caraskimberly@yahoo.com>
2017-01-21 12:23:52 -0500

    I successfully read in the Abbott and Costello's Who's on First skit. I am now trying to get sentiment of each user individually.

--- Dialog.py
+++ Dialog.py
@@ -1,5 +1,8 @@
+import nltk.corpus
+from nltk.text import Text
 class Dialog:
-    def __init__(self, st, c, d, t, s, r, sa ):
+
+    def __init__(self, st, c, d, t, s, r, sa):
         self.speaker = s
         self.recipient = r
         self.stamp = st
@@ -8,4 +11,29 @@ class Dialog:
         self.time= t
         self.startsAt = sa
 
+    def do(self):
+        print "Hello World"
+
+if __name__ == '__main__':
+    fi = open("AbbottCostello.txt")
+    raw = fi.read()
+    tokens = nltk.word_tokenize(raw)
+    t = nltk.Text(tokens)
+    t.similar("")
+    fi.close()
+    fi = open("AbbottCostello.txt")
+    cnt = 1
+    speakers = {"Abbott:": [], "Costello:": []}
+    for line in fi.readlines():
+        a = line.split()
+        di = None
+        if len(a) < 1:
+            continue
+        elif a[0] == "Abbott:":
+            di = Dialog(None, line[1:], None, cnt, a[0], "Costello:", None)
+        elif a[0] == "Costello:":
+            di = Dialog(None, line[1:], None, cnt, a[0], "Abbott:", None)
+        speakers[a[0]].append(di)
+        cnt += 1
+    fi.close()
 
--- TestAnalysis.py
+++ TestAnalysis.py
@@ -1,1 +1,21 @@
-import 
+import nltk
+import Dialog
+import datetime
+#d = datetime()
+#di = Dialog(None, cont, None, None, sp, ra, )
+fi = open("AbbottCostello.txt")
+ab = []
+ca = []
+cnt = 1
+speakers = {"Abbott:": [], "Costello:":[]}
+for line in fi.readlines():
+    a = line.split()
+    if a[0] == "Abbott:":
+        di = Dialog(None, line[1:], None, cnt, a[0], "Costello:", None)
+    if a[0] == "Costello:":
+        di = Dialog(None, line[1:], None, cnt, a[0], "Abbott:", None)
+    speakers[a[0]].append(di)
+    print a
+
+
+fi.close()
