a969f1e682b3fe5d40208652963aed3cf0a39666
kcaras6 <caraskimberly@yahoo.com>
2017-01-27 20:15:33 -0500

    Got the data of two people in the converstation. Still trying to determine how to compare them

--- Analyze.py
+++ Analyze.py
@@ -1,6 +1,9 @@
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import Dialog
 import TextParsing
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.cm as cm
 
 class Analyze:
     def __init__(self):
@@ -41,17 +44,33 @@ class Analyze:
         return lines
 
     #TODO: getDifference does not work
-    def getDifference (self, tp, speakerArray):
+    def getDifference (self, tp, speakerArray, sentiment):
+        funcArr = []
         diff = []
-        for sp in tp.speakerDict.keys():
+        if len(speakerArray) < 2:
+            print "please enter in two or more speakers"
+            return
+        k = 0
+        for sp in speakerArray:
+            funcArr.append([[],[]])
             for i in tp.speakerDict[sp]:
                 sentence = tp.dialogues[i].content
-                #print sentence
                 # ss is a dictionary containing the compound, negative (neg) and positive sentiment rating of a single line of Abbott's
                 ss = self.sid.polarity_scores(sentence)
-                #for k in sorted(ss):
-                #    print ('{0} : {1}, '.format(k, ss[k]))
-                diff.append((sentence, [ss['compound'], ss['neg'], ss['neu'], ss['pos']]))
+                #print sentence, i
+                funcArr[k][0].append(float(i))
+                funcArr[k][1].append(float(ss[sentiment]))
+                #diff.append((sentence, [ss['compound'], ss['neg'], ss['neu'], ss['pos']]))
+            k += 1
+
+        fig = plt.figure()
+        ax1 = fig.add_subplot(111)
+        colors = cm.rainbow(np.linspace(0, 1, len(funcArr)))
+        for i in range(len(funcArr)):
+            ax1.scatter(funcArr[i][0], funcArr[i][1], s=10,c=colors[i], marker="s", label=speakerArray[i])
+        plt.legend(loc='upper left')
+        plt.show()
+
 
     """
      Calculates the average compound, negative, neutral and positive scores of all speakers in the conversation to determine
@@ -77,3 +96,4 @@ if __name__ == '__main__':
     print a.getAverageScores(tp)
     #print a.getSentimentData(tp, "Tempus")
     print a.getConversationSentiment(tp)
+    a.getDifference(tp, ["Tempus", "Bunnycrusher"], "pos")
