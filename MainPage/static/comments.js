5fa333e86e24705e1b7959c25a25ac49cd0835a4
Colin Luff <cluff3@gatech.edu>
2017-02-18 01:43:40 +0000

    added commenting!

--- MainPage/static/comments.js
+++ MainPage/static/comments.js
@@ -1,3 +1,23 @@
+var counter = 1;
+
 function comment(){
-  console.log("Hello World");
+
+	if(counter>10){
+    alert("Only 10 comments allowed");
+    return false;
+	}
+
+	var newTextBoxDiv = $(document.createElement('div'))
+	     .attr("id", 'TextBoxDiv' + counter);
+
+	newTextBoxDiv.after().html('<span id="draggable'+ counter+' class="draggable">*' +
+	      '<input type="text" name="textbox' + counter +
+	      '" id="resizable' + counter +
+        '" class="ui-state-active">'+'</span>');
+
+	newTextBoxDiv.appendTo("#TextBoxesGroup");
+
+	//$(".resizable").resizable();
+
+	counter++;
 }
