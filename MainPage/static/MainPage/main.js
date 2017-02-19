a2bbe34139dd9e6456d42d0e1851546f4fbe2aa1
Jackie Zhu <jackie.zhu21@gmail.com>
2017-02-19 10:23:18 -0500

    updated UI

--- MainPage/static/MainPage/main.js
+++ MainPage/static/MainPage/main.js
@@ -20,20 +20,20 @@ $(document).ready(function() {
 });
 
 
-$("about_header").click(function () {
-    alert("HIIIII");
+// $("about_header").click(function () {
+//     alert("HIIIII");
 
-    $about_header = $(this);
-    //getting the next element
-    $about_content = $about_header.next();
-    //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
-    $about_content.slideToggle(500, function () {
-        //execute this after slideToggle is done
-        //change text of header based on visibility of content div
-        $about_header.text(function () {
-            //change text based on condition
-            return $about_content.is(":visible") ? "Collapse" : "Expand";
-        });
-    })(jQuery);
+//     $about_header = $(this);
+//     //getting the next element
+//     $about_content = $about_header.next();
+//     //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
+//     $about_content.slideToggle(500, function () {
+//         //execute this after slideToggle is done
+//         //change text of header based on visibility of content div
+//         $about_header.text(function () {
+//             //change text based on condition
+//             return $about_content.is(":visible") ? "Collapse" : "Expand";
+//         });
+//     })(jQuery);
 
-});
+// });
