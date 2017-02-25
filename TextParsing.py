7b46b260b3fd94ae8df6a59e8051bc33f5524621
kPossible007 <caraskimberly@yahoo.com>
2017-02-24 21:03:17 -0500

    I added functionality to calculate the word frequency and added a text class object from nltk to TextParsing.

--- TextParsing.py
+++ TextParsing.py
@@ -1,10 +1,29 @@
 import Dialog
 import Person
+import nltk
+from nltk import word_tokenize
+"""
+fdist = FreqDist(samples)   ======> create a frequency distribution containing the given samples
+fdist[sample] += 1          ======> increment the count for this sample
+fdist['monstrous'] 	        ======> count of the number of times a given sample occurred
+fdist.freq('monstrous')     ======> frequency of a given sample
+fdist.N() 	                ======> total number of samples
+fdist.most_common(n) 	    ======> the n most common samples and their frequencies
+for sample in fdist: 	    ======> iterate over the samples
+fdist.max() 	            ======> sample with the greatest count
+fdist.tabulate() 	        ======> tabulate the frequency distribution
+fdist.plot() 	            ======> graphical plot of the frequency distribution
+fdist.plot(cumulative=True) ======> cumulative plot of the frequency distribution
+fdist1 |= fdist2 	        ======> update fdist1 with counts from fdist2
+fdist1 < fdist2 	        ======> test if samples in fdist1 occur less frequently than in fdist2
+"""
+
 class TextParsing:
     def __init__(self, str):
         self.dialogues = []
         self.speakerDict = {}
         self.speakerToClass = {}
+        self.text = None
         self.parse(str)
 
 #TODO Complete this method
@@ -21,6 +40,7 @@ class TextParsing:
         fh = open(str, 'r')
         content = fh.readlines()
         lines = []
+        tokens = []
         for line in content:
             lines.append(line)
         fh.close()
@@ -62,11 +82,30 @@ class TextParsing:
                 i += 2
                 line = lines[i]
                 startsAt = line[5:(len(line) - 12)]
-
+                tokens.extend(word_tokenize(content.strip()))
                 self.dialogues.append(Dialog.Dialog(timestamp, content, date, time, speaker, recipients, startsAt))
                 self.speakerDict[speaker].append(index)
                 index += 1
                 i += 4
             else:
                 i += 1
+        self.text = nltk.Text(tokens)
 
+
+
+if __name__ == '__main__':
+    tp = TextParsing("exampleData.rtf")
+    #Concordance tells you all of the times the word is in said in the conversation along with its context
+    #print tp.text.concordance('kill')
+    #This positional information can be displayed using a dispersion plot.
+    # Each stripe represents an instance of a word, and each row represents the entire text.
+    #tp.text.dispersion_plot(["kill", "bunny"])
+    #counts the number of time the word kill was said in the conversation
+    #print tp.text.count("kill")
+    fdist1 = nltk.FreqDist(tp.text)
+    #gets the first 50 commen words that were spoken in the conversation
+    #returns a list of tuples. ex: ('Yea', 19)
+    #print fdist1.most_common(50)
+    #fdist1.plot(50, cumulative=True)
+    #Returns a list of words that only appear once
+    #print fdist1.hapaxes()
