5ea1efa06b80c607541ee6f723766207bb778f03
Matt Gordon <coltfanman@gmail.com>
2017-03-30 17:27:57 -0400

    Working person page

--- MainPage/static/tags.js
+++ MainPage/static/tags.js
@@ -222,4 +222,13 @@ window.onload = function() {
         myPlot.data = data;
         Plotly.redraw(myPlot);
     });
+
+    var form = $("#personform");
+    var people = JSON.parse($("#pInfo").text());
+    for (i in people) {
+      var element = $(document.createElement('option'));
+      element.val(JSON.stringify(people[i])).text(i);
+      $("#personform").append(element);
+      console.log(people[i]);
+    }
 };
--- MainPage/urls.py
+++ MainPage/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^main/$', views.main, name='main'),
     url(r'^results', views.results, name='results'),
+    url(r'^person', views.person, name='person'),
     url(r'^tags', views.tags),
 ]
